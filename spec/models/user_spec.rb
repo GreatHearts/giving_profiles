292766dbb14a5023941a18c3a54334816d14d7a4
Alex Rincon <alejandro.rincon9@gmail.com>
2019-03-04 21:22:43 -0800

    Add has_many relationship tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,14 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   subject { User.new(name: "Foo Bar") }
 
+  it { should have_many(:user_favorite_organizations).dependent(:destroy) }
+  it { should have_many(:donations).dependent(:destroy) }
+  it do
+    should have_many(:favorite_organizations)
+      .through(:user_favorite_organizations)
+      .source(:organization)
+  end
+
   it { should validate_presence_of(:fb_id) }
   it { should validate_uniqueness_of(:fb_id) }
   it { should validate_uniqueness_of(:email) }
