2ba5f7b6a6018e2ac96ae55d5fd97e71b886e426
Mark Donald Amobi <markdamobi@gmail.com>
2019-03-09 22:02:51 -0600

    properly align code

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,4 @@
-(function() {
+(function(){
   $(document).ready(function(){
     setDonationBars();
   });
@@ -8,14 +8,14 @@
     var donations_bars = $(".donations-bars");
     grid_template_columns = "";
 
-     $(".donations-bars > .donation-bar").each(function(index){
+    $(".donations-bars > .donation-bar").each(function(index){
       _that = $(this);
       grid_template_columns = grid_template_columns + " " +  _that.data("percent") + "%";
     });
 
-     $(".donations-bars").css("grid-template-columns", grid_template_columns);
+    $(".donations-bars").css("grid-template-columns", grid_template_columns);
 
-     // this is a hack to adjust space because column grid space pushes div outside of container. 
+    // this is a hack to adjust space because column grid space pushes div outside of container. 
     // Will change this during proper implementation.
     $(".donations-bars").css("padding-right", (($(".donations-bars > div").length -1)* 1) + "px");  
   } 
