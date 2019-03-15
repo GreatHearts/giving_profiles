f39c5bf14e4a597aefa65e427d341185b54a299d
Emily Spieler <emilyspieler1@gmail.com>
2019-03-14 20:32:24 -0500

    controller removal

--- test/controllers/giving_profiles_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class GivingProfilesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get giving_profiles_index_url
-    assert_response :success
-  end
-
-end
