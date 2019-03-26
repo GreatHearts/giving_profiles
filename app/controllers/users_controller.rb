2b40d37161c8d5a6f04bb3b5220553707a3d78d1
Mark Donald Amobi <markdamobi@gmail.com>
2019-03-26 17:12:38 -0500

    remove some iteration logic from user_fav_org view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,21 @@
 class UsersController < ApplicationController
   helper_method :cause_logos ##will delete when we get real org logos.
-  before_action :get_user, only: [:show, :edit]
+  before_action :get_user, :get_donations_by_causes, only: [:show, :edit]
 
   def show
     @badges = @user.badges
-    @donations_by_causes = @user.donations_by_causes
   end
 
   def edit 
-    @donations_by_causes = @user.donations_by_causes
+    @user_fav_orgs = @user.user_favorite_organizations[0..3]
   end
 
   def get_user
     @user = User.includes(donations: :organization, user_favorite_organizations: :organization).find(params[:id])
+  end
+
+  def get_donations_by_causes
+    @donations_by_causes = @user.donations_by_causes
   end
 
   def cause_logos
