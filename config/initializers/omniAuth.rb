2e83fefabfacdb24a07f7242867de92d379bcb85
rashad <rashadmad@gmail.com>
2019-05-04 17:36:43 -0500

    Add omniauth.rb with id and secret

--- config/initializers/omniAuth.rb
+++ config/initializers/omniAuth.rb
@@ -1,4 +1,4 @@
-OmminAuth.config.logger = Rails.logger
+OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['2676296335717134'], ENV['5bc8a2cc1ed1b5b69b2d915f4090bd76']
