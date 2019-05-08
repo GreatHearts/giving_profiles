39c6ad4c277606fe00f408f206af63c145e9e0c1
rashad <rashadmad@gmail.com>
2019-05-08 18:00:01 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get '/about' => 'home#about'
   get '/policy' => 'home#policy'
 
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
+
   resources :users, only: [:show, :edit]
 end
