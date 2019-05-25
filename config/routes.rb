aabc4a6dd25bef8bce75930cb142920ae3b4f243
rashad <rashadmad@gmail.com>
2019-05-24 20:16:49 -0500

    create a session controller and add facebook link

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'home#login'
   get '/about' => 'home#about'
   get '/policy' => 'home#policy'
+  get 'auth/facebook/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
 
   resources :users, only: [:show, :edit]
 end
