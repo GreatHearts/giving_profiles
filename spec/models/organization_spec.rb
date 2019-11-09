ad7f7831cfc05fdd48cce9b3d6269eccc8c8a05d
Brittney Braxton <brittney.braxton@edovo.com>
2019-11-09 15:12:12 -0600

    Merge conflicts

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe Organization, type: :model do
 
   describe "Searching" do
     before do
-      @organization = Organization.create(name: Faker::Stargate.planet, org_type: Organization.org_types.first.first, location: "#{Faker::Address.city}, IL")
+      @organization =
+          Organization.create(
+              name: Faker::Company::name,
+              org_type: Organization.org_types.first.first,
+              location: "#{Faker::Address.city}, IL"
+          )
     end
 
     it "return results if there are matches " do
@@ -13,6 +18,8 @@ RSpec.describe Organization, type: :model do
       org_type_result = PgSearch.multisearch(organization.org_type)
       location_result = PgSearch.multisearch(organization.location)
 
+      expect(name_result.present?).to be true
+      expect(name_result.map(&:searchable).include?(organization)).to be true
       expect(name_result.first.searchable.name).to eq @organization.name
       allow(@organization).to receive(:org_type).and_return("animals")
 
